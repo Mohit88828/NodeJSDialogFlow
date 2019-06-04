0328d61802377f4ce3ab7d60678a640d31783b4f
Wisethink <mayanksriva.soft@gmail.com>
2019-06-04 11:50:32 +0530

    second commit

--- index.js
+++ index.js
@@ -14,20 +14,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var temp = {
-    google: {
-      expectUserResponse: true,
-      richResponse: {
-        items: [
-          {
-            simpleResponse: {
-              textToSpeech: "this is a simple response"
-            }
-          }
-        ]
-      }
-    }
-  };
+  
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
@@ -35,9 +22,9 @@ restService.post("/echo", function(req, res) {
       ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    payload: temp,
-    data: temp,
-    fulfillmentText: "Sample response",
+    //payload: temp,
+    //data: temp,
+    //fulfillmentText: "Sample response",
     speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
