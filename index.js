8bb51386c6824a9b3bf694e09321c3d7e0697bee
Wisethink <mayanksriva.soft@gmail.com>
2019-06-05 13:09:56 +0530

    let update

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ restService.post("/audio", function(req, res) {
     //Speech Synthesis Markup Language 
     case "music one":
       speech =
-        '<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+        //'<speak><audio src="https://actions.google.com/sounds/v1/cartoon/slide_whistle.ogg">did not get your audio file</audio></speak>';
+		'<img src="https://i2.wp.com/www.quantemol.com/wordpress/wp-content/uploads/2017/09/image-demo-b.png"/>';
       break;
     case "music two":
       speech =
